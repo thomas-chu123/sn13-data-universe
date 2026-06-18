6086d9240505a43eb4d88f4dc5b2963220b24f20
skynet <chu_liang_han@hotmail.com>
2026-06-18 17:05:20 +0800

    port change

--- pm2_start.sh
+++ pm2_start.sh
@@ -1,1 +1,1 @@
-pm2 start python --name sn13-miner -- ./neurons/miner.py --wallet.name skynet --wallet.hotkey skynet
+pm2 start python --name sn13-miner -- ./neurons/miner.py --axon.port 10022 --wallet.name skynet --wallet.hotkey skynet
